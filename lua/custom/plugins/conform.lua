37c74921c263c2b7116869c744c4f77585896467
makes <1053209+makes@users.noreply.github.com>
2023-12-27 23:13:20 +0200

    update conform.nvim config

--- lua/custom/plugins/conform.lua
+++ lua/custom/plugins/conform.lua
@@ -4,22 +4,28 @@
 
 return {
   'stevearc/conform.nvim',
-  event = "BufWritePre", -- load the plugin before saving
-  keys = {
-    {
-      "<leader>f",
-      function() require("conform").format({ lsp_fallback = true }) end,
-      desc = "Format",
-    },
-  },
-  opts = {
-    formatters_by_ft = {
-      python = { 'ruff_fix', 'ruff_format' },
-      markdown = { 'inject' },
-    },
-    -- format_on_save = {
-    --   timeout_ms = 500,
-    --   lsp_fallback = true,
-    -- },
-  }
+
+  config = function()
+    require("conform").setup({
+      formatters_by_ft = {
+        python = { 'ruff_fix', 'ruff_format' },
+        markdown = { 'inject' },
+      },
+      -- format_on_save = {
+      --   timeout_ms = 500,
+      --   lsp_fallback = true,
+      -- },
+    })
+
+    vim.keymap.set(
+      'n',
+      '<leader>f',
+      function()
+        require("conform").format({
+          lsp_fallback = true,
+        })
+      end,
+      { desc = 'Auto-format.' }
+    )
+  end
 }
